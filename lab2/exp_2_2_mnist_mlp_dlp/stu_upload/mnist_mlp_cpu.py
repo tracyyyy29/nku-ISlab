c08379115d118b9827e3c9e22ada82ecf5b1023f
TANG CUIXI <154045364+tracyyyy29@users.noreply.github.com>
2025-05-16 14:23:49 +0800

    Update mnist_mlp_cpu.py

--- lab2/exp_2_2_mnist_mlp_dlp/stu_upload/mnist_mlp_cpu.py
+++ lab2/exp_2_2_mnist_mlp_dlp/stu_upload/mnist_mlp_cpu.py
@@ -162,7 +162,7 @@ class MNIST_MLP(object):
 
 
 def build_mnist_mlp(param_dir='weight.npy'):
-    h1,h2,e=64,16,10
+    h1,h2,e=128，64，20
     mlp=MNIST_MLP(batch_size=10000,hidden1=h1,hidden2=h2,max_epoch=e)
     mlp.load_data()
     mlp.build_model()
